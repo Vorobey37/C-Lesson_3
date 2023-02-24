5a23f4c25a10f215caf0a3ed2ad4de2626d8b62d
Abvorobey <abvorobey@gmail.com>
2023-02-24 23:02:42 +0300

    Таблица умножения

---
+++ for_in_for/Program.cs
@@ -0,0 +1,11 @@
+﻿// Таблица умножения
+Console.Clear();
+
+for (int i=2; i<=10; i++)
+{
+    for (int j=2; j<=10; j++)
+    {
+        Console.WriteLine ($"{i}*{j}={i*j}");
+    }
+    Console.WriteLine();
+}
